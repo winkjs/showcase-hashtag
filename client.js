aecce8a80cc16441b43909785c0a30a9f745ab15
Prateek Saxena <prtksxna@gmail.com>
2020-05-05 13:20:22 +0530

    feat: add table with results

--- client.js
+++ client.js
@@ -1,9 +1,25 @@
-const getSentiment = function (hashtag) {
+var showResults = function (tweets) {
+  var $results = document.getElementById('results');
+  tweets.forEach((tweet) => {
+    var row = '<tr>';
+
+    row += '<td>' + tweet.user + '</td>';
+    row += '<td>' + tweet.tweet + '</td>';
+    row += '<td>' + tweet.normalizedSentimentScore + '</td>';
+
+    row += '</tr>';
+    $results.innerHTML += row;
+  });
+
+}
+
+var getSentiment = function (hashtag) {
   fetch('https://showcase-serverless.now.sh/twitter-sentiment?hashtag='+hashtag)
     .then(function (res) {
       return res.json();
     }).then(function (d) {
       console.log(d);
+      showResults(d.tweets);
     })
 }
 
