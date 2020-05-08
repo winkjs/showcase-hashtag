53207ade2f753eb9014999514a2adbd95e815d0e
Prateek Saxena <prtksxna@gmail.com>
2020-05-08 08:32:46 +0530

    feat: add colored circles for sentiment score

--- client.js
+++ client.js
@@ -2,12 +2,15 @@ var showResults = function (tweets) {
   var $results = document.getElementById('results');
   tweets.forEach((tweet) => {
     var row = '<tr>';
+    var cssClass = (tweet.normalizedSentimentScore > 0 ) ? 'results__score--positive' : 'results__score--negative';
+    cssClass = (tweet.normalizedSentimentScore === 0 ) ? '':cssClass;
+    console.log(cssClass);
 
     row += '<td>' +
         '<img class="results__avatar" src="' + tweet.avatar + '" alt="'+ tweet.user +'">'
       '</td>';
     row += '<td class="results__tweet"><strong>' + tweet.user + '</strong>: ' + tweet.tweet + '</td>';
-    row += '<td class="results__score">' + tweet.normalizedSentimentScore.toFixed(1) + '</td>';
+    row += '<td class="results__score '+ cssClass + '">' + tweet.normalizedSentimentScore.toFixed(1) + '</td>';
 
     row += '</tr>';
     $results.innerHTML += row;
